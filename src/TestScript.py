3c6cba3acfa495c426f4b114f2b36128e8f64acf
AlexandreBanks6 <65726682+AlexandreBanks6@users.noreply.github.com>
2024-03-25 15:39:04 -0700

    Updates

--- src/TestScript.py
+++ src/TestScript.py
@@ -1,10 +1,17 @@
 import glm
 import numpy as np
 from include import HandEye
+import random
 def normalize_2d(matrix):
     norm = np.linalg.norm(matrix)
     matrix = matrix/norm  # normalized matrix
     return matrix
+
+def generateRotation(theta):
+    R=np.array([[np.cos(theta),-np.sin(theta),0],
+               [np.sin(theta),np.cos(theta),0],
+               [0,0,1]])
+    return R
 
 hand_eye_obj=HandEye.HandEye()
 
@@ -23,13 +30,31 @@ hand_eye_obj=HandEye.HandEye()
          #   [0,0,0,1]])
 #a,a_prime=hand_eye_obj.HomoToDualQuat(A)
 
-A=[normalize_2d(np.random.rand(4,4))]
-B=[normalize_2d(np.random.rand(4,4))]
+transA=normalize_2d(np.random.randn(1,3))
+RA=generateRotation(random.uniform(0,1)*np.pi)
+transB=normalize_2d(np.random.randn(1,3))
+RB=generateRotation(random.uniform(0,1)*np.pi)
+A=np.identity(4)
+B=np.identity(4)
+A[0:3,0:3]=RA
+A[0:3,3]=transA
+B[0:3,0:3]=RB
+B[0:3,3]=transB
+A=[A]
+B=[B]
 print("A: "+str(A))
 print("B: "+str(B))
 for i in range(0,3):
-    new_A=np.random.rand(4,4)
-    new_B=np.random.rand(4,4)
+    transA=normalize_2d(np.random.randn(1,3))
+    RA=generateRotation(random.uniform(0,1)*np.pi)
+    transB=normalize_2d(np.random.randn(1,3))
+    RB=generateRotation(random.uniform(0,1)*np.pi)
+    new_A=np.identity(4)
+    new_B=np.identity(4)
+    new_A[0:3,0:3]=RA
+    new_A[0:3,3]=transA
+    new_B[0:3,0:3]=RB
+    new_B[0:3,3]=transB
 
     A.append(normalize_2d(new_A))
     B.append(normalize_2d(new_B))
