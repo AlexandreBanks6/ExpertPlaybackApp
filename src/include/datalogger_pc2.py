67abd0a0232339ad3a90e386cd9fd36f81083528
AlexandreBanks6 <65726682+AlexandreBanks6@users.noreply.github.com>
2024-08-19 21:13:16 -0700

    Update datalogger_pc2.py

--- src/include/datalogger_pc2.py
+++ src/include/datalogger_pc2.py
@@ -14,7 +14,12 @@ from include import utils
 
 #File Column Titles:
 repeat_string=["Tx","Ty","Tz","R00","R01","R02","R10","R11","R12","R20","R21","R22"] #First number is row of R, second number is column
-MOTION_HEADER=["Time","si_T_psm1"]+repeat_string+["si_T_psm3"]+repeat_string+["si_T_ecm"]+repeat_string+["joint_vars_psm1"]+["q1","q2","q3","q4","q5","q6","jaw"]+["joint_vars_psm3"]+["q1","q2","q3","q4","q5","q6","jaw"]
+MOTION_HEADER_PC1=["Task Time","PC1 Time","","PC2 Time","s_T_psm1"]+repeat_string+["s_T_psm3"]+repeat_string+\
+    ["cart_T_ecm"]+repeat_string+["ecm_T_psm1"]+repeat_string+["ecm_T_psm3"]+repeat_string+\
+            ["psm1_joints"]+["q1","q2","q3","q4","q5","q6","jaw"]+["psm3_joints"]+["q1","q2","q3","q4","q5","q6","jaw"]+\
+            ["ecm_joints"]+["q1","q2","q3","q4"]
+
+MOTION_HEADER_PC2=["PC2 Time","Frame #","lc_T_s"]+repeat_string+["rc_T_s"]+repeat_string+["is_gaze_calib"]
 
 
 class DataLogger_PC2:
@@ -23,7 +28,7 @@ class DataLogger_PC2:
         self.record_filename=None
         self.app=app
 
-    def initRecording(self,root_path):
+    def initRecording_PC1(self,root_path):
 
         #Initialize a new CSV file to save to
         file_count=1
@@ -41,7 +46,7 @@ class DataLogger_PC2:
         #Store all transforms that do not change
         with open(self.record_filename,'w',newline='') as file_object:
             writer_object=csv.writer(file_object)
-            writer_object.writerow(MOTION_HEADER)
+            writer_object.writerow(MOTION_HEADER_PC1)
 
             #Writing si_T_lci (scene to left camera initial)
             writer_object.writerow(['lci_T_si'])
@@ -67,7 +72,100 @@ class DataLogger_PC2:
             ecm_T_rc_list=utils.convertHomogeneousToCSVROW(ecm_T_rc_numpy)
             writer_object.writerow(["",""]+ecm_T_rc_list)
 
+            #Writing cart_T_ecmi (robot base to ecm initial)
+            writer_object.writerow(['cart_T_ecmi'])
+            cart_T_ecmi_numpy=np.array(glm.transpose(self.app.cart_T_ecmi).to_list(),dtype='float32')
+            cart_T_ecmi_list=utils.convertHomogeneousToCSVROW(cart_T_ecmi_numpy)
+            writer_object.writerow(["",""]+cart_T_ecmi_list)
+
             file_object.close()
+
+    def convertHomogeneousToCSVROW(self,transform):
+        #Input: 4x4 numpy array for homogeneous transform
+        #Output: 12x1 string list with: "Tx","Ty","Tz","R00","R01","R02","R10","R11","R12","R20","R21","R22"
+
+        string_list=[str(transform[0,3]),str(transform[1,3]),str(transform[2,3]),\
+                    str(transform[0,0]),str(transform[0,1]),str(transform[0,2]),\
+                    str(transform[1,0]),str(transform[1,1]),str(transform[1,2]),\
+                    str(transform[2,0]),str(transform[2,1]),str(transform[2,2])]
+        
+        
+        return string_list
+    
+    def writeRow_PC1(self,task_time,pc1_time,pc2_time,s_T_psm1,s_T_psm3,cart_T_ecm,ecm_T_psm1,ecm_T_psm3,psm1_joints,psm3_joints,ecm_joints):
+
+
+        #s_T_psm1 & s_T_psm3
+        if s_T_psm1 is not None:
+            s_T_psm1_numpy=np.array(glm.transpose(s_T_psm1).to_list(),dtype='float32')
+            s_T_psm1_list=self.convertHomogeneousToCSVROW(s_T_psm1_numpy)
+        else:
+            s_T_psm1_list=["NaN"]*12
+
+        if s_T_psm3 is not None:
+            s_T_psm3_numpy=np.array(glm.transpose(s_T_psm3).to_list(),dtype='float32')
+            s_T_psm3_list=self.convertHomogeneousToCSVROW(s_T_psm3_numpy)
+        else:
+            s_T_psm3_list=["NaN"]*12
+
+
+        #cart_T_ecm
+        if cart_T_ecm is not None:
+            cart_T_ecm_numpy=np.array(glm.transpose(cart_T_ecm).to_list(),dtype='float32')
+            cart_T_ecm_list=self.convertHomogeneousToCSVROW(cart_T_ecm_numpy)
+        else:
+            cart_T_ecm_list=["NaN"]*12
+
+
+        #ecm_T_psm1 & ecm_T_psm3
+        if ecm_T_psm1 is not None:
+            ecm_T_psm1_numpy=np.array(glm.transpose(ecm_T_psm1).to_list(),dtype='float32')
+            ecm_T_psm1_list=self.convertHomogeneousToCSVROW(ecm_T_psm1_numpy)
+        else:
+            ecm_T_psm1_list=["NaN"]*12
+
+        if ecm_T_psm3 is not None:
+            ecm_T_psm3_numpy=np.array(glm.transpose(ecm_T_psm3).to_list(),dtype='float32')
+            ecm_T_psm3_list=self.convertHomogeneousToCSVROW(ecm_T_psm3_numpy)
+        else:
+            ecm_T_psm3_list=["NaN"]*12
+
+        
+        #psm1_joints and psm3_joints
+
+        if psm1_joints is not None:
+            joint_list_psm1=[str(num) for num in psm1_joints]
+        else:
+            joint_list_psm1=["NaN"]*7
+
+        if psm3_joints is not None:
+            joint_list_psm3=[str(num) for num in psm3_joints]
+        else:
+            joint_list_psm3=["NaN"]*7
+
+        
+        #ecm_joints
+        if ecm_joints is not None:
+            joint_list_ecm=[str(num) for num in ecm_joints]
+        else:
+            joint_list_ecm=["NaN"]*4
+
+        
+        #Write the row
+        row_to_write=[str(task_time),str(pc1_time),str(pc2_time),""]+s_T_psm1_list+[""]+s_T_psm3_list+[""]+\
+        cart_T_ecm_list+[""]+ecm_T_psm1_list+[""]+ecm_T_psm3_list+[""]+joint_list_psm1+[""]+joint_list_psm3+[""]+\
+        joint_list_ecm
+
+        with open(self.record_filename,'a',newline='') as file_object:
+            writer_object=csv.writer(file_object)
+            writer_object.writerow(row_to_write)
+
+
+
+
+
+
+    
 
     
 
